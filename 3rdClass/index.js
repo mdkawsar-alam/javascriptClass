051659129bf3a16fae6de6770eda856e5c1e378a
mdkawsar-alam <mdkawsaralam537@gmail.com>
2024-11-24 22:13:41 +0600

    first commit

---
+++ 3rdClass/index.js
@@ -0,0 +1,59 @@
+//  const taka= 2580 ;
+// const age= 25
+//  const year= 2024
+
+//   console.log(taka+" "+ age +' '+ year);
+  
+
+// var age= true
+
+// console.log(typeof age);
+
+
+// const message='  this is my 'programing' class'
+
+//  var data= {name:"anic", age:25}
+//   console.log(typeof data);
+  
+//   console.log(data);
+  
+
+//  var gpa=1.5
+
+
+// if (gpa >= 4.0){
+//     console.log("A+");
+    
+// }
+// else if( gpa>= 3.5){
+//     console.log("A-");
+    
+// }
+// else if( gpa >=3.0){
+//     console.log("B+");
+    
+// }
+// else if( gpa>=2.5){
+//     console.log("B-");
+    
+// }
+// else(
+//     console.log('fail')
+    
+// );
+// 
+  
+
+
+// function  hello(a,b){
+
+// console.log(b+a);
+
+// console.log(1565);
+// return 
+    
+// }
+
+// hello(5,52)
+
+
