d181776f1c21a4a925f0275975ee3cbc5968e87d
mdkawsar-alam <mdkawsaralam537@gmail.com>
2024-11-27 17:15:22 +0600

    conditions and for loop  oparetors

---
+++ 4thClass/index.js
@@ -0,0 +1,143 @@
+
+// var a =20
+//  var b=30
+//  var c= '30'
+
+//  console.log(b!==c);
+
+
+//  var x=50
+//  var y=50
+
+//  console.log(x<=y);
+ 
+
+// var x=50
+
+// if (x<=60 && x>=52 ) {
+//     console.log("bangladesh");
+    
+// } else {
+//     console.log("india");
+    
+    
+// }
+
+
+// var x=50
+
+// if (x<=60 || x>=52 ) {
+//     console.log("bangladesh");
+    
+// } else {
+//     console.log("india");
+    
+    
+// }
+
+
+// var a= prompt()
+
+// if (a=="1") {
+//     console.log("one");
+    
+    
+// } else if(a=="2") {
+//     console.log("two");
+    
+    
+// }
+// else if(a=="3") {
+//     console.log("three");
+    
+    
+// }
+
+// else if(a=="4") {
+//     console.log("four");
+    
+    
+// }
+
+// else if(a=="5") {
+//     console.log("five");
+    
+    
+// }
+
+// else if(a=="6") {
+//     console.log("six");
+    
+    
+// }
+
+// else if(a=="7") {
+//     console.log("seven");
+    
+    
+// }
+
+// else if(a=="8") {
+//     console.log("eight");
+    
+    
+// }
+// else if(a=="9") {
+//     console.log("nine");
+    
+    
+// }
+// else{
+//     console.log("number is not valid");
+    
+// }
+//  var a= prompt()
+
+// switch (a) {
+//     case '1':console.log("one");
+//     break
+//     case '2':console.log("two");
+//     break
+//     case '3':console.log("three");
+//     break
+//     case '4':console.log("four");
+//     break
+//     case '5':console.log("five");
+//     break
+//     case '6':console.log("six");
+//     break
+//     case '7':console.log("seven");
+//     break
+//     case '8':console.log("eight");
+//     break
+//     case '9':console.log("nine");
+//     break
+//     default:
+//         console.log(" this number is not valid");
+        
+//        break
+   
+// }
+
+
+
+
+//    for log 
+
+
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+
+for (var a=0 ;a<= 10; a=a+2  ){
+    // document.write(`<h1> hello world </h1>`)
+    console.log(a);
+    
+
+
+    
+}
+document.write(`<h1> End </h1>`)
