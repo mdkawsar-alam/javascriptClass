d28c8b17d267ac48938095d94da0996b46995885
mdkawsar-alam <mdkawsaralam537@gmail.com>
2024-12-01 18:03:24 +0600

    add 5th class

---
+++ 5thClass/index.js
@@ -0,0 +1,111 @@
+
+//  const num=20
+
+//  if (num >= 80) {
+//     console.log("A+");
+    
+    
+//  }
+//  else if (num >=70) {
+//     console.log("A-");
+    
+    
+//  }
+//  else if (num>=60) {
+//     console.log("B");
+    
+    
+//  }
+//  else if (num>=50) {
+//     console.log("B-");
+    
+    
+//  }
+//  else if (num>=40) {
+//     console.log("C");
+    
+    
+//  }
+//  else{
+//     console.log("Fail");
+    
+//  }
+ 
+
+//  for lop
+//  for(let num=1;num<=100;num=num+3){
+//     console.log("boss" ,num);
+    
+//  }
+
+// while lop
+
+
+//   for(; ;){
+//      console.log("boss" )
+//   }
+
+
+
+// while (false) {
+//     let num= Math.ceil(Math.random()*50)
+//      console.log(num);
+     
+    
+//     if (num==10)break
+        
+    
+// }
+
+
+
+// do{
+// console.log("bangladesh");
+
+// }
+// while (false) {
+// console.log("BANGLADESH");
+
+    
+// }
+
+
+// Array
+
+//  const st1="rakib"
+//  const st2="ovi"
+// const st3="roni"
+//  const st4="tuhin"
+
+// array
+//  const students=['rakib', 'ovi', 'roni', 'tuhin ']
+const st1={
+    name:"ovi",
+    age:22,
+    class:11
+}
+const st2={
+    name:"sakib",
+    age:22,
+    class:11
+}
+const st3={
+    name:"rakib",
+    age:22,
+    class:11
+}
+ const allStudent=[st1,st2,st3]
+
+
+ console.log(allStudent)
+// for (let i = 0; i<students.length; i++) {
+//     const element = students[i];
+//    console.log(element);
+
+    
+     
+    
+// }
+//  console.log("hello world");
+ 
+ 
