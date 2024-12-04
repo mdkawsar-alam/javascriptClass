9a18cb86d5405cc30e8b06934230538fa55e2925
mdkawsar-alam <mdkawsaralam537@gmail.com>
2024-12-04 17:49:29 +0600

    add 6th class

---
+++ 6thClass/index.js
@@ -0,0 +1,33 @@
+
+// const myNumber=[ 1,5, 7, 11, 15,8];
+
+// let sum=0;
+
+//  for (let i = 0; i < myNumber.length; i++) {
+
+//    // sum=sum+myNumber[i]
+    
+//     sum += myNumber[i];
+//     console.log(sum);
+    
+//  }
+//  console.log(sum);
+ 
+
+// function myfun(a,b){
+//    console.log("bangladesh");
+//    console.log("hello");
+//    return a+b
+ 
+   
+   
+// }
+
+
+// console.log(myfun(20,40));
+
+// (function evan(){
+//    console.log(21%2);
+   
+//    return 
+// })
