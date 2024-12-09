413710330705bbe6f3e620734a166cb91235c78e
mdkawsar-alam <mdkawsaralam537@gmail.com>
2024-12-09 15:32:00 +0600

    add js class 7

---
+++ 7thClass/index.js
@@ -0,0 +1,107 @@
+// function myFn(){
+//    console.log("hello");
+    
+// }
+
+// myFn()
+
+// const myfunction= function(){
+
+//  }
+
+// const myFn=()=>{
+//     console.log("bangladesh");
+    
+
+// }
+// myFn()
+
+//   
+
+//  array operations
+
+
+
+
+// const newfruits = fruits.slice(0,3)
+
+// console.log(newfruits);
+
+
+
+// document.write("<h1> f</h1>")
+// console.log(fruits[0]);
+// console.log(numbers);
+
+// numbers.push(6);
+// console.log(numbers);
+
+// fruits.push('Orange');
+//  console.log(fruits);
+ 
+
+
+// const newNumber= numbers.map((number)=>(number= number+2))
+// console.log(newNumber);
+
+
+// const evenNumbers= numbers.filter(number => number % 2 !== 0);
+// console.log(evenNumbers);
+
+
+
+
+//   reducer
+// const sum = numbers.reduce((acc, num) => acc + num,20);
+// console.log(sum); 
+
+//  find
+
+
+
+// const found = numbers.find(num => num === 3);
+//   console.log(found); 
+
+// numbers.sort((a, b) => b - a);
+// console.log(numbers);
+
+
+
+
+
+// every 
+
+// const allPositive = numbers.every(num => num > 0);
+// console.log(allPositive);
+//  some
+// const allPositive = numbers.some(num => num < 0);
+// console.log(allPositive);
+ 
+const numbers = [1, 2, 3,4, 5,6,8,10,7,13,];
+
+//   concat
+// const moreNumbers = [6, 7, 8];
+
+// const combined = numbers.concat(moreNumbers);
+// console.log(combined);
+
+// join
+
+// const fruits = ['Apple', 'Banana', 'Cherry','mango'];
+// const joined = fruits.join(', ');
+// console.log(joined); 
+// console.log(fruits);
+
+// flat
+
+// const nestedArray = [1, [2, 3], [4, [5,[ 6]]]];
+
+// const flat = nestedArray.flat(3); 
+// console.log(flat); 
+
+//  remove duplicate
+
+// const duplicateArray = [1, 2, 2, 3, 3, 4];
+
+// const unique = [...new Set(duplicateArray)];
+// console.log(unique); 
