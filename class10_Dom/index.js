dcc28055a6dd83779b5d95299bbc34864391d67e
mdkawsar-alam <mdkawsaralam537@gmail.com>
2024-12-18 17:10:03 +0600

    add dom class

---
+++ class10_Dom/index.js
@@ -0,0 +1,42 @@
+
+//  const king= document.getElementById('king');
+//  const soldier= document.getElementsByClassName('soldier')
+//   console.log(soldier,);
+//  const  h1 =document.getElementsByTagName('h1')
+//  console.log(h1);
+//   const name=document.getElementsByName('name')
+//   console.log(name);
+// const id = document.querySelector('#love'); 
+// console.log(id);
+
+// const ektaDiv = document.querySelector('.love'); 
+// console.log(ektaDiv);  // সবার মধ্যে প্রথমটাকে প্রেম নিবেদন করবে
+// const sobDiv = document.querySelectorAll('.love');  
+// console.log(sobDiv);  // সবগুলাকে এক লাইন করে দাঁড় করায়
+
+
+// const maBaba = document.getElementById('maBaba');
+// maBaba.innerText='Dad mom'
+
+// const papa = document.getElementById('papa');
+// papa.innerHTML= '<h1>papa </h1>'
+
+
+
+
+// const partyDiv = document.getElementById('party');
+
+//   const newMember = document.createElement('p')
+
+//   newMember.innerText = "আমিও পার্টিতে এসেছি!";
+
+// partyDiv.appendChild(newMember)
+
+// console.log(partyDiv.innerHTML);
+
+
+
+
+
+
+
