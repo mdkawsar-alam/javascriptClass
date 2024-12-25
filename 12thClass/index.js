f82f6957df3b0c10a0aea26af16a50ed84190098
mdkawsar-alam <mdkawsaralam537@gmail.com>
2024-12-25 17:02:51 +0600

    add 12 class

---
+++ 12thClass/index.js
@@ -0,0 +1,17 @@
+// const team = document.getElementById('team');
+
+
+// const player1 = document.getElementById('player1');
+
+// player1.remove();  
+
+// team.removeChild(player1);
+
+
+
+
+
+
+function handleDelete (player){
+    player.remove();  
+}
